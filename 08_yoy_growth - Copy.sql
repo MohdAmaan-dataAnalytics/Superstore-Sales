95712ac4bbe8e7d397dec25edb69c9374117bdb7
Mohd Amaan <mohdammu086@gmail.com>
2026-09-18 16:39:50 +0530

    Delete 08_yoy_growth - Copy.sql

--- 08_yoy_growth - Copy.sql
+++
@@ -1,9 +0,0 @@
-USE superstore_analysis;
-
-SELECT YEAR(order_date) AS years, MONTH(order_date) AS months, 
-	SUM(sales) AS month_sales,
-	LAG (SUM(sales),12) OVER(ORDER BY YEAR(order_date) , MONTH(order_date)) AS some_month_last_year,
-    SUM(sales) - LAG (SUM(sales),12) OVER(ORDER BY YEAR(order_date) , MONTH(order_date)) AS sales_defferent
-FROM superstore_sales
-GROUP BY MONTH(order_date), YEAR(order_date)
-ORDER BY YEAR(order_date), MONTH(order_date);
